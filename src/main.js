b5a38b69e292b856513451e7a0e3bf40128054ce
Oleksii Zelenko <52129260+AlexeyZelenko@users.noreply.github.com>
2020-07-06 09:42:32 +0300

    1

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import router from './router'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+import { firestorePlugin } from 'vuefire'
+
+export const db = firebase.initializeApp({ projectId: 'applicantswork-master' }).firestore()
+
 
 Vue.config.productionTip = false
 
+Vue.use(firestorePlugin)
 
 new Vue({
   vuetify,
